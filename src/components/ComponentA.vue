4c9c6ef9c5b65cb22ccd40a250a5b69afc72b562
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-04-06 00:01:01 +0700

    update ComponentA

--- src/components/ComponentA.vue
+++ src/components/ComponentA.vue
@@ -1,20 +1,52 @@
 <template>
-  <div class="hello">
-    <h1>{{ message }}</h1>
-  </div>
+    <div class="hello">
+        <h1>{{ message }}</h1>
+        <p>Time now is: {{ currentDateTime }}</p>
+        <p v-if="isShow">This line is hide/show</p>
+        <ol>
+            <li v-for="post in posts" v-bind:key="post.title">
+                <p class="bold">{{ post.title }}</p>
+                <p>{{ post.content }}</p>
+            </li>
+        </ol>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'ComponentA',
-  props: {
-    message: String
-  }
-}
+    export default {
+        name: 'ComponentA',
+        props: {
+            message: String
+        },
+        data() {
+            return {
+                currentDateTime: Date.now().toLocaleString(),
+                isShow: true,
+                posts: [
+                    {
+                        title: 'Why VueJs ?',
+                        content: 'Vuejs is a frontend framework'
+                    },
+                    {
+                        title: 'VueJs vs ReactJs ?',
+                        content: 'Vuejs vs ReactJs are frontend framework'
+                    },
+                    {
+                        title: 'VueJs call request to NodeJs',
+                        content: 'Vuejs call api to NodeJs via axios, fetch'
+                    }
+                ]
+            }
+        }
+    }
 </script>
 
 <style scoped>
-h1{
-  color: red;
-}
+    h1 {
+        color: red;
+    }
+
+    p.bold {
+        font-weight: bold;
+    }
 </style>
