02a72e6793c67f42f1be69847ae763b0a36385e6
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-04-06 10:30:08 +0700

    use bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <ComponentA message="Hello Vuejs"/>
-    <ComponentB message="Hello Vuejs"/>
-    <ComponentC message="Hello Vuejs"/>
+  <div id="container">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+    <PostComponent message="List posts"/>
   </div>
 </template>
 
 <script>
-import ComponentA from './components/ComponentA'
-import ComponentB from './components/ComponentB'
-import ComponentC from './components/ComponentC'
+// import ComponentA from './components/ComponentA'
+// import ComponentB from './components/ComponentB'
+// import ComponentC from './components/ComponentC'
+import PostComponent from './components/PostComponent'
 
 export default {
   name: 'app',
   components: {
-    ComponentA,
-    ComponentB,
-    ComponentC
+    // ComponentA,
+    // ComponentB,
+    // ComponentC,
+    PostComponent
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
